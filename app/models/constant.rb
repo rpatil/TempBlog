33be9649278a84faddef468d75c3b5cf0112fd1d
Rahul Patil <rahulpatil_scs@yahoo.co.in>
2014-11-24 20:00:04 +0530

    Change in SeedsDB

--- app/models/constant.rb
+++ app/models/constant.rb
@@ -6,27 +6,66 @@ class Constant < ActiveRecord::Base
 
 
   TUTORIAL_TITLE_0 = "The Basics of Node.js Streams"
-  TUTORIAL_TITLE_1 = "Sass Multiple Arguments, Lists or Arglist"
-  TUTORIAL_TITLE_2 = "Getting Started with Google Maps Recipes"
-  TUTORIAL_TITLE_3 = "A Box of Tricks for Building Responsive Email"
-  TUTORIAL_TITLE_4 = "Getting Started with FigDice"
-	TUTORIAL_TITLE_5 = "Mini-chat with Rails and Server-Sent Events"
-	TUTORIAL_TITLE_6 = "KPIs for Freelancers"
-	TUTORIAL_TITLE_7 = "Flask for the Masses"
-	TUTORIAL_TITLE_8 = "Yii 2.0 ActiveRecord Explained"
-	TUTORIAL_TITLE_9 = "Developing for the WordPress.org Plugin Directory"
-
-The official WordPress.org Plugin Directory is home to an amazing collection of useful plugins, it’s usually the first place WordPress users look when seeking to extend the functionality of their sites. The Plugin Directory offers great opportunities for developers, however there are some guidelines you need to follow. A base plugin will help you to easily and quickly get started in the development process.
 	TUTORIAL_SHORT_INTRO_0 = "Node.js is asynchronous and event driven in nature. As a result, it’s very good at handling I/O bound tasks. If you are working on an app that performs I/O operations, you can take advantage of the streams available in Node.js. So, let’s explore Streams in detail and understand how they can simplify I/O."
-	TUTORIAL_SHORT_INTRO_1 = "I do a lot of Sass code reviews. Along the same lines, I like to read frameworks’ code. Needless to say, I have seen a lot of things, both good and bad, when it comes to Sass.If there is something that always make me grind my teeth when I read other people’s Sass, it’s seeing mixins and functions using lists instead of variable arguments, or using lists instead of multiple named arguments. "
-	TUTORIAL_SHORT_INTRO_2 = "There are many ways to add a Google Map to a web page. Google offers a variety of map types whose use depends essentially on your needs."
-	TUTORIAL_SHORT_INTRO_3 = "In my previous article on newsletter authoring we’ve seen how a handful of tricks can make a huge difference in how your email displays in different clients."
-	TUTORIAL_SHORT_INTRO_4 = "Amongst the many templating systems out there, most work in pretty much the same way; variables are “injected” using some syntax or another, be it curly braces, percentage signs or whatever that library’s convention happens to be. They’ll usually have basic control structures, such as if...then and, of course, iteration."
-	TUTORIAL_SHORT_INTRO_5 = "Recently, I’ve penned a couple of articles on building a chat application with Rails (Mini-Chat with Rails and Realtime Mini-Chat with Rails and Faye – check out the working demo here). In those articles, I explained how to build a simple mini-chat application and make it truly asynchronous using two alternatives: AJAX polling (less preferred) and Web Sockets."
-	TUTORIAL_SHORT_INTRO_6 = "Tech startups (and startups in general) are typically fiends for metrics, measuring everything and tracking their growth. Freelancers, on the other hand, are notorious for being less-than-organized. There’s so much work that comes with running a business-of-one that it’s easy to let measurement slip through the cracks — which can easily lead to your business downfall. Enter: your key performance indicators. "
-	TUTORIAL_SHORT_INTRO_7 = ""
-	TUTORIAL_SHORT_INTRO_8 = ""
-	TUTORIAL_SHORT_INTRO_9 = ""
 
+  TUTORIAL_TITLE_1 = "Sass Multiple Arguments, Lists or Arglist"
+	TUTORIAL_SHORT_INTRO_1 = "I do a lot of Sass code reviews. Along the same lines, I like to read frameworks’ code. Needless to say, I have seen a lot of things, both good and bad, when it comes to Sass.If there is something that always make me grind my teeth when I read other people’s Sass, it’s seeing mixins and functions using lists instead of variable arguments, or using lists instead of multiple named arguments. "
+	
+  TUTORIAL_TITLE_2 = "Getting Started with Google Maps Recipes"
+	TUTORIAL_SHORT_INTRO_2 = "There are many ways to add a Google Map to a web page. Google offers a variety of map types whose use depends essentially on your needs."
+
+  TUTORIAL_TITLE_3 = "A Box of Tricks for Building Responsive Email"
+	TUTORIAL_SHORT_INTRO_3 = "In my previous article on newsletter authoring we’ve seen how a handful of tricks can make a huge difference in how your email displays in different clients."
+
+  TUTORIAL_TITLE_4 = "Getting Started with FigDice"
+	TUTORIAL_SHORT_INTRO_4 = "Amongst the many templating systems out there, most work in pretty much the same way; variables are “injected” using some syntax or another, be it curly braces, percentage signs or whatever that library’s convention happens to be. They’ll usually have basic control structures, such as if...then and, of course, iteration."
+
+	TUTORIAL_TITLE_5 = "Mini-chat with Rails and Server-Sent Events"
+	TUTORIAL_SHORT_INTRO_5 = "Recently, I’ve penned a couple of articles on building a chat application with Rails (Mini-Chat with Rails and Realtime Mini-Chat with Rails and Faye – check out the working demo here). In those articles, I explained how to build a simple mini-chat application and make it truly asynchronous using two alternatives: AJAX polling (less preferred) and Web Sockets."
+
+	TUTORIAL_TITLE_6 = "KPIs for Freelancers"
+	TUTORIAL_SHORT_INTRO_6 = "Tech startups (and startups in general) are typically fiends for metrics, measuring everything and tracking their growth. Freelancers, on the other hand, are notorious for being less-than-organized. There’s so much work that comes with running a business-of-one that it’s easy to let measurement slip through the cracks — which can easily lead to your business downfall. Enter: your key performance indicators."
+
+	TUTORIAL_TITLE_7 = "Flask for the Masses"
+	TUTORIAL_SHORT_INTRO_7 = "About a year ago, I was looking into another programming language outside of PHP. I’d been an active PHP developer for 10 years, and, to be honest, doing apps only in PHP gets a bit boring. It not having any standardized libraries or frameworks didn’t help either"
+
+	TUTORIAL_TITLE_8 = "Yii 2.0 ActiveRecord Explained"
+	TUTORIAL_SHORT_INTRO_8 = "The ActiveRecord class in Yii provides an object oriented interface (aka ORM) for accessing database stored data. Similar structures can be found in most modern frameworks like Laravel, CodeIgniter, Smyfony and Ruby. Today, we’ll go over the implementation in Yii 2.0 and I’ll show you some of the more advanced features of it."
+
+	TUTORIAL_TITLE_9 = "Developing for the WordPress.org Plugin Directory"
+	TUTORIAL_SHORT_INTRO_9 = "The official WordPress.org Plugin Directory is home to an amazing collection of useful plugins, it’s usually the first place WordPress users look when seeking to extend the functionality of their sites. The Plugin Directory offers great opportunities for developers, however there are some guidelines you need to follow. A base plugin will help you to easily and quickly get started in the development process."
+
+	TUTORIAL_TITLE_10 = "Building an Internationalized Blog with FigDice"
+	TUTORIAL_SHORT_INTRO_10 = "In part one of this two-part series I started looking at FigDice, a PHP templating system that takes a slightly different approach to most. So far we’ve put together a very simple example website using Figdice. We’ve implemented a couple of pages, a Twitter feed and some template partials."
+
+	TUTORIAL_TITLE_11 = "Using the Shopify API with WordPress"
+	TUTORIAL_SHORT_INTRO_11 = "WordPress is extremely popular with small businesses, but more and more product businesses are also opening up e-commerce stores using Shopify. It’s possible to keep an existing info site and use of the Shopify API with a custom page template to pull a feed of products from Shopify into a WordPress site.There is a working feed of products on this demo page, which pulls from a demo Shopify store, and the end result looks like this"
+
+	TUTORIAL_TITLE_12 = "Pagination and SEO: Red Flags and Best Practices	"
+	TUTORIAL_SHORT_INTRO_12 = "For any webmaster of a large site, content pagination is a topic of interest. When you have a vast amount of content it’s difficult to avoid pagination. In addition to your pagination concerns from a usability point of view, the SEO aspects are also worth considering.Pagination is not rocket science but as far as SEO is concerned there are a few things that you need to know. Before we delve into these specifics, let’s first clarify the different types of pagination and the problems they can lead to."
+
+	TUTORIAL_TITLE_13 = "Junior, a Front-end Framework for HTML 5 Mobile Apps"
+	TUTORIAL_SHORT_INTRO_13 = "Junior is a front-end framework for creating HTML 5 mobile apps. Depending on external libraries like modernizer, zepto, zepto flickable, lodash and backbone, it creates mobile apps with a native look and feel by utilizing Ratchet CSS UI components."
+
+	TUTORIAL_SHORT_INTRO_14 = "Luke Wroblewski talked about the need for Mobile First designs back in 2011 and shared some early data in 2012, much before we saw such a major shift in web usage patterns. Two years on, with smartphones and tablets replacing PCs dramatically, even at the workplace, it’s alarming to see most designers and developers still stuck designing the traditional way – for large screen desktops – and still talking about progressive enhancement versus graceful degradation."
+	TUTORIAL_TITLE_14 = "Making a Case for Mobile First Designs"
+
+	TUTORIAL_TITLE_15 = "On Our Radar This Week: Watch Apple, and Intuitive Animations"
+	TUTORIAL_SHORT_INTRO_15 = "Apple have released development kit information for their new Apple Watch, with info on how the device will work (at least initially) and details about getting your iPhone app ready for Apple Watch. Needless to say, there has been a lot of talk about this, and among them are these initial impressions for WatchKit."
+
+	TUTORIAL_TITLE_16 = "Building OctoberCMS Plugins: Google Analytics"
+	TUTORIAL_SHORT_INTRO_16 = "OctoberCMS is one of the rising stars in content management systems, and as in every CMS we need plugins to extend the functionality. In this article, we’re going to go through the basics of creating a plugin for October CMS."
+
+	TUTORIAL_TITLE_17 = "Building a Today Extension for iOS 8"
+	TUTORIAL_SHORT_INTRO_17 = "A lot of welcome enhancements have been introduced in iOS 8, one of the most powerful new features is the concept of extensions. Extensions provide several ways to share functionality and data with iOS or other apps, from your own app."
+
+	TUTORIAL_TITLE_18 = "Passing Your Sass to Someone Sass-less"
+	TUTORIAL_SHORT_INTRO_18 = "Sass is like Pringles: once you pop, you can’t stop. After using Sass for a couple projects, picking up a vanilla CSS project feels weird. Where are my variables? Why can’t I nest things? I just want this one mixin… In all of our Sass-isfaction, it’s actually pretty easy to forget that there developers out there who don’t or can’t use a preprocessor. And we shouldn’t forget them or disparage them!"
+
+	TUTORIAL_TITLE_19 = "Mozilla Introduces Firefox Developer Edition"
+	TUTORIAL_SHORT_INTRO_19 = "Just a few days ago, Mozilla celebrated the 10th anniversary of Firefox, which, back in 2004 initiated the next level in the browser wars by disrupting the Internet Explorer monopoly that Microsoft had established for a number of years."
+
+	TUTORIAL_TITLE_20 = "Practical OOP: Building a Quiz App – Bootstrapping"
+	TUTORIAL_SHORT_INTRO_20 = "At a certain point of my development as a PHP programmer, I was building MVC applications by-the-book, without understanding the ins-and-outs. I did what I was told: fat model, thin controller. Don’t put logic in your views."
 
 end
